7befb28019de8f0c48ee7c930966c3d34e4b8eca
Vineeth <vineeth.bodipudi@imaginato.com>
2020-02-25 17:20:24 +0530

    Initial Commit

---
+++ CocoaFramework/CocoaFramework.h
@@ -0,0 +1,19 @@
+//
+//  CocoaFramework.h
+//  CocoaFramework
+//
+//  Created by mac-0008 on 25/02/20.
+//  Copyright © 2020 Mac-0008. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaFramework.
+FOUNDATION_EXPORT double CocoaFrameworkVersionNumber;
+
+//! Project version string for CocoaFramework.
+FOUNDATION_EXPORT const unsigned char CocoaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaFramework/PublicHeader.h>
+
+
