549247881c791dba2db31c109386dae5045f1046
Vineeth <vineeth.bodipudi@imaginato.com>
2020-02-25 17:25:28 +0530

    - Added a project

---
+++ CocoaFramework/Name.swift
@@ -0,0 +1,18 @@
+//
+//  Name.swift
+//  CocoaFramework
+//
+//  Created by mac-0008 on 25/02/20.
+//  Copyright © 2020 Mac-0008. All rights reserved.
+//
+
+import Foundation
+
+
+open class Name {
+    
+    static public func getName() -> String {
+        
+        return "Vineeth"
+    }
+}
